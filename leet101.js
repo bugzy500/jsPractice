a0ce7480bca5a58aa985e0a22e69adeb93d5ec17
bugzy500 <arpit96garg@outlook.com>
2024-06-22 02:17:34 +0530

    trees

---
+++ leet101.js
@@ -0,0 +1,36 @@
+// symmetric tree
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root.left && !root.right)
+        return true
+
+    if(!root.left || !root.right)
+        return false
+
+    const stack1 = [root.left]
+    const stack2 = [root.right]
+
+    while(stack1.length && stack2.length){
+        let temp1 = stack1.pop()
+        let temp2 = stack2.pop()
+
+        if(!temp1 && !temp2)
+            continue
+
+        if(!temp1 || !temp2 || temp1.val !== temp2.val)
+            return false
+
+        stack1.push(temp1.left)
+        
+        stack1.push(temp1.right)
+        stack2.push(temp2.right)
+        
+        stack2.push(temp2.left)
+    }
+
+    return true
+};
--- leet226.js
+++ leet226.js
@@ -4,6 +4,62 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
-    
+var invertTree = function (root) {
+
+    if(!root)
+        return []
+
+    const stack = []
+    stack.push(root)
+
+    function reverseNodes(node) {
+
+        let temp = node.left
+        node.left = node.right
+        node.right = temp
+ 
+    }
+
+    while(stack.length){
+        let temp = stack.shift()
+
+        reverseNodes(temp)
+
+        if(temp.left)
+            stack.push(temp.left)
+        
+        if(temp.right)
+            stack.push(temp.right)
+    }
+
+    console.log(root)
+
 };
+
+const tree = {
+    val: 3,
+    left: {
+        val: 9,
+        left: null,
+        right: {
+            val: 10,
+            left: null,
+            right: null
+        }
+    },
+    right: {
+        val: 20,
+        left: {
+            val: 15,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 7,
+            left: null,
+            right: null
+        }
+    }
+}
+
+invertTree(tree)
